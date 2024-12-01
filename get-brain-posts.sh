f934849289a52b12550ace39f0c4c2629480caa5
kapott <kapott@aivd.33mail.com>
2024-12-01 12:00:15 +0100

    Iron stuff out..

--- get-brain-posts.sh
+++ get-brain-posts.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 # Vars this script needs in order to function:
-#SRC_DIR="$HOME/Documents/Brain/Blog/posts"
+#SRC_DIR="$HOME/Documents/Brain/Blog"
 #DEST_DIR="$PWD/content/posts"
 
 source .env
 rm -rf content/posts/*
-rsync -avh $SRC_DIR/* $DEST_DIR/ --delete
-rsync -avh $SRC_DIR/../images/* static/images/ --delete
-# ./transform-links.sh
-# python3 transform-links.py $DEST_DIR
-#rsync -avh $SRC_DIR/images/* $PWD/static/images/
+rsync -avh $SRC_DIR/posts.* $DEST_DIR/ --delete
+rsync -avh $SRC_DIR/images/* static/images/ --delete
